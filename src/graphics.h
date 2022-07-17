26e66e5ad7c15c40a17c8e94a8cf6e069a21d31a
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2022-07-18 03:26:07 +1000

    Geometry Data Structures and 3D Functions

--- src/graphics.h
+++ src/graphics.h
@@ -10,8 +10,12 @@ typedef struct Screen
 
 void draw_char(int x, int y, char value);
 void draw_line(int x1, int y1, int x2, int y2, char value);
+void draw_tri(int x1, int y1, int x2, int y2, int x3, int y3, char value);
 
 void setup_screen(void);
 void show_screen(void);
+void clear_screen(void);
+int screen_width(void);
+int screen_height(void);
 
 #endif /* GRAPHICS_H_ */
