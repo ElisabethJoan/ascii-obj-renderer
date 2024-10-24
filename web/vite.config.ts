26c9da3968c5cae6ea73d824bb75576d64a1cb86
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2024-10-25 05:01:47 +1000

    changed cors isolation method attempt

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+        react(),
+        crossOriginIsolation()
+    ],
   base: "https://elisabeth.github.io/ascii-obj-renderer/",
 });
